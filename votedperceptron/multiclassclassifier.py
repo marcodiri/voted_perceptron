fixed classes score calculation

votedperceptron/multiclassclassifier.py
@@ -83,13 +83,12 @@ class MulticlassClassifier:
             # continue
 
             eval_binary_classifier = self.binary_classifiers[eval_class]
-            s = 0
-            v_per_x = np.dot(eval_binary_classifier.vectors_list[0], x)
-            for i in range(len(eval_binary_classifier.vectors_list)):
-                v_per_x += eval_binary_classifier.mistaken_labels[i]*\
-                           eval_binary_classifier.kernel(eval_binary_classifier.mistaken_examples[i], x)
 
-                s += eval_binary_classifier.weights[i] * np.sign(v_per_x)
+            # after getting the class with max score, compute the voted prediction
+            s = 0
+            for v_per_x, c in zip(eval_binary_classifier.v_per_xs,
+                                  eval_binary_classifier.weights):
+                s += c * np.sign(v_per_x)
 
             prediction = np.sign(s)
             bool_ = prediction == 1
@@ -107,6 +106,7 @@ class MulticlassClassifier:
         full_predictions = []
         if process_count is not None and process_count > 1:
             # slit up input list for the workers
+            print("Splitting dataset for {} processes".format(process_count))
             split_input = np.array_split(test_list, process_count)
 
             with Pool(processes=process_count) as pool:

votedperceptron/votedperceptron.py
@@ -71,12 +71,25 @@ class VotedPerceptron:
         # each VotedPerceptron instance represent a label
         # so compute the score
         score = None
+        self.v_per_xs = [np.dot(self.vectors_list[0], x)]
         if method == 'last':
-            score = np.dot(self.vectors_list[-1], x)
+            for me, ml, c in zip(self.mistaken_examples,
+                                 self.mistaken_labels,
+                                 self.weights):
+                self.v_per_xs.append(self.v_per_xs[-1] + ml * self.kernel(me, x))
+            score = self.v_per_xs[-1]
         elif method == 'vote':
-            score = sum(c * np.sign(np.dot(v, x))
-                        for v, c in zip(self.vectors_list, self.weights))
+            score = 0
+            for me, ml, c in zip(self.mistaken_examples,
+                                 self.mistaken_labels,
+                                 self.weights):
+                self.v_per_xs.append(self.v_per_xs[-1] + ml * self.kernel(me, x))
+                score += c * np.sign(self.v_per_xs[-1])
         elif method == 'avg':
-            score = sum(c * np.dot(v, x)
-                        for v, c in zip(self.vectors_list, self.weights))
+            score = 0
+            for me, ml, c in zip(self.mistaken_examples,
+                                 self.mistaken_labels,
+                                 self.weights):
+                self.v_per_xs.append(self.v_per_xs[-1] + ml * self.kernel(me, x))
+                score += c * self.v_per_xs[-1]
         return score